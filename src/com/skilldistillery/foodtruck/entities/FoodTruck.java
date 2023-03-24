3faa5f375ce83c54061bf92ce7954e178a2d2c8f
jennkal <jennkalkowski@gmail.com>
2023-03-24 17:36:03 -0600

    init FoodTruck bits

---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.foodtruck.entities;
+
+//create many food trucks, blueprint.
+public class FoodTruck {
+	private String name;
+	
+	
+	//constructors
+	
+	//get/set methods
+	
+	//toString method
+}
